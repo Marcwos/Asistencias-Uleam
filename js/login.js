557897103545c1761cdab14a1c3909eeecf817b2
Marcwos <ubituna22@gmail.com>
2025-05-31 12:51:53 -0500

    Creacion de login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function login() {
+  const usuario = document.getElementById("usuario").value;
+  const clave = document.getElementById("clave").value;
+
+  // Lógica de login simulada
+  if (usuario === "admin" && clave === "1234") {
+    alert("Ingreso correcto");
+    window.location.href = "materias.html";
+  } else {
+    alert("Credenciales incorrectas");
+  }
+}
