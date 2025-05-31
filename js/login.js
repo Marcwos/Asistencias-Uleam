772e5be2c21ec1daaed883479cc22c8e9f338fa5
Marcwos <ubituna22@gmail.com>
2025-05-31 13:25:40 -0500

    Creacion de componente para reutilizar header en todas las paginas, ademas se corregio la ruta en login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ function login() {
   // Lógica de login simulada
   if (usuario === "admin" && clave === "1234") {
     alert("Ingreso correcto");
-    window.location.href = "materias.html";
+    window.location.href = "materia.html";
   } else {
     alert("Credenciales incorrectas");
   }
